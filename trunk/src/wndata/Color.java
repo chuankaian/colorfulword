ef1e9287d5ec974062e10e8c0b8ee6f447b4a44b
Zero.Ztz@gmail.com <Zero.Ztz@gmail.com@e9bebc9f-c1d8-17fd-fb22-d278c5164839>
2011-06-25 12:21:55 +0000

    Add class named Color.java

--- trunk/src/wndata/Color.java
+++ trunk/src/wndata/Color.java
@@ -8,12 +8,13 @@ public class Color implements ColorStoreInfo{
 	private byte[] b;
 	private byte[] a;
 	private Color(){
-	}
-	public Color(char[] data){
 		r=new byte[STORENUM];
 		g=new byte[STORENUM];
 		b=new byte[STORENUM];
 		a=new byte[STORENUM];
+	}
+	public Color(char[] data){
+		this();
 		for (int i=0;i<STORENUM;++i){
 			r[i]=(byte)(data[i*2]&255);
 			g[i]=(byte)(data[i*2]>>8);
@@ -22,10 +23,35 @@ public class Color implements ColorStoreInfo{
 		}
 	}
 	public char[] serialize(){
-		res=new char[STORENUM*2];
+		char[] res=new char[STORENUM*2];
 		for (int i=0;i<STORENUM;++i){
-			res[i*2]=(char)g[i]<<8|r[i];
-			res[i*2+1]=(char)a[i]<<8|b[i];
+			res[i*2]=(char)(g[i]<<8|r[i]);
+			res[i*2+1]=(char)(a[i]<<8|b[i]);
 		}
+		return res;
+	}
+	public void setR(int index,byte r){
+		this.r[index]=r;
+	}
+	public void setG(int index,byte g){
+		this.g[index]=g;
+	}
+	public void setB(int index,byte b){
+		this.b[index]=b;
+	}
+	public void setA(int index,byte a){
+		this.a[index]=a;
+	}
+	public byte getR(int index){
+		return r[index];
+	}
+	public byte getG(int index){
+		return g[index];
+	}
+	public byte getB(int index){
+		return b[index];
+	}
+	public byte getA(int index){
+		return a[index];
 	}
 }
