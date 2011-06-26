c7d58b94249a168f6f83d7df6fc7c3be6cf448fe
Zero.Ztz@gmail.com <Zero.Ztz@gmail.com@e9bebc9f-c1d8-17fd-fb22-d278c5164839>
2011-06-26 12:09:13 +0000

    finish coding coloring

--- trunk/src/coloring/ColorManager.java
+++ trunk/src/coloring/ColorManager.java
@@ -1,8 +1,42 @@
 package coloring;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 import wndata.*;
 
 public class ColorManager implements ColorStoreInfo{
+	static Map<String,Float> weight = new HashMap<String,Float>(); 
+	static{
+		weight.put("antonym",new Float(0.01f));
+		weight.put("hypernym",new Float(0.1f));
+		weight.put("instance hypernym",new Float(0.1f));
+		weight.put("hyponym",new Float(0.3f));
+		weight.put("instance hyponym",new Float(0.3f));
+		weight.put("member holonym",new Float(0.05f));
+		weight.put("substance holonym",new Float(0.05f));
+		weight.put("part holonym",new Float(0.05f));
+		weight.put("member meronym",new Float(0.15f));
+		weight.put("substance meronym",new Float(0.15f));
+		weight.put("part meronym",new Float(0.15f));
+		weight.put("attribute",new Float(0.075f));
+		weight.put("derivationally related form",new Float(0.6f));
+		weight.put("domain of synset - TOPIC",new Float(0.05f));
+		weight.put("member of this domain - TOPIC",new Float(0.15f));
+		weight.put("domain of synset - REGION",new Float(0.05f));
+		weight.put("member of this domain - REGION",new Float(0.15f));
+		weight.put("domain of synset - USAGE",new Float(0.05f));
+		weight.put("member of this domain - USAGE",new Float(0.15f));
+		weight.put("entailment",new Float(0.2f));
+		weight.put("cause",new Float(0.1f));
+		weight.put("also see",new Float(0.01f));
+		weight.put("verb group",new Float(0.1f));
+		weight.put("similar to",new Float(0.7f));
+		weight.put("particle of verb",new Float(0.6f));
+		weight.put("pertainym (for adjectives) or derived from adjective (for adverbs)",new Float(0.6f));
+	}
 	private static ColorManager instance;
 	private ColorManager(){}
 	public static ColorManager getSingleton(){
@@ -12,14 +46,45 @@ public class ColorManager implements ColorStoreInfo{
 		return instance;
 	}
 	public boolean setColor(Synset synset, Color color){
+		final Map<Synset,Float> map=new HashMap<Synset,Float>();
+		PriorityQueue<Synset> pq=new PriorityQueue<Synset>(1, new Comparator<Synset>(){
+			public int compare(Synset arg0, Synset arg1){
+				if (map.get(arg0)>map.get(arg1))
+					return -1;
+				else if (map.get(arg0)==map.get(arg1))
+					return 0;
+				else
+					return 1;
+			}
+		});
+		map.put(synset, new Float(255));
+		pq.offer(synset);
+		while (!pq.isEmpty()){
+			Synset u=pq.poll();
+			Float ui=map.get(u);
+			color.setA((char) ui.intValue());
+			DataManager.getSingleton().getColor(u).addColor(color);
+			SynsetPointer[] sp=synset.getPointers();
+			for (int i=0;i<sp.length;++i){
+				Synset v=DataManager.getSingleton().getSynset(sp[i].getSynsetOffset(), sp[i].getPartOfSpeech());
+				Float vi=ui.floatValue()*weight.get(sp[i].getPointerSymbol().getDescription());
+				if (vi>=1){
+					Float vi_=map.get(v);
+					if (vi_==null || vi_<vi){
+						map.put(v, vi);
+						if (vi_==null)
+							pq.offer(v);
+					}
+				}
+			}
+		}
 		return true;
 	}
 	public Color getColor(Synset synset){
 		float r=0;
 		float g=0;
 		float b=0;
-		DataManager p=new DataManager();//Zero
-		Colors colors=p.getColor(synset);
+		Colors colors=DataManager.getSingleton().getColor(synset);
 		int totWeight=0;
 		for (int i=0;i<STORENUM;++i)
 			totWeight+=colors.getA(i);
